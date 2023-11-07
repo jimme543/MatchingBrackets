a4e16b30e7d00f5e7711e3051a4bc585f6661afb
James Criddle <jamcridd@gmail.com>
2023-11-07 00:01:36 -0700

    complete challenge, remove implicit usings

--- MatchingBracketsChallenge/MatchingBrackets.cs
+++ MatchingBracketsChallenge/MatchingBrackets.cs
@@ -1,10 +1,41 @@
-﻿namespace MatchingBracketsChallenge
+﻿using System.Collections.Generic;
+
+namespace MatchingBracketsChallenge
 {
     public class MatchingBrackets
     {
+        private const char START_BRACKET = '<';
+        private const char END_BRACKET = '>';
+
         public bool HasMatchingBrackets(string s)
         {
             bool result = true;
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char character in s)
+            {
+                if (character == START_BRACKET)
+                {
+                    stack.Push(character);
+                }
+                else if (character == END_BRACKET)
+                {
+                    if (stack.TryPop(out char poppedCharacter))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+
+            if (stack.Count > 0)
+            {
+                result = false;
+            }
 
             return result;
         }
